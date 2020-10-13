6cb4123eada40c877533dc8f722d04be7109421d
Matt Keeter <matt.j.keeter@gmail.com>
2020-10-13 17:33:28 -0400

    Got glfw window opening working

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,23 @@
 const std = @import("std");
-const glfw3 = @cImport({
+const glfw = @cImport({
     @cInclude("GLFW/glfw3.h");
 });
 
 pub fn main() anyerror!void {
-    std.debug.warn("All your codebase are belong to us.\n", .{});
+    if (glfw.glfwInit() != glfw.GLFW_TRUE) {
+        std.debug.panic("Could not initialize glfw", .{});
+    }
+
+    const window = glfw.glfwCreateWindow(640, 480, "hello", null, null);
+    defer glfw.glfwDestroyWindow(window);
+    if (window == null) {
+        var err_str: [*c]u8 = null;
+        const err = glfw.glfwGetError(&err_str);
+        std.debug.panic("Failed to open window: {} ({})", .{err, err_str});
+    }
+
+    glfw.glfwMakeContextCurrent(window);
+    while (glfw.glfwWindowShouldClose(window) == 0) {
+        glfw.glfwWaitEvents();
+    }
 }

b5467049fa9ccef53bf15f977aa66120375b8359
Matt Keeter <matt.j.keeter@gmail.com>
2020-11-14 22:06:28 -0500

    More work on fpHlAttrs

--- extern/futureproof.h
+++ extern/futureproof.h
@@ -18,8 +18,23 @@ struct fpAtlasUniforms {
     uint32_t glyph_height;
     uint32_t glyph_advance;
 };
+
+#define FP_FLAG_BOLD          (1 << 0)
+#define FP_FLAG_ITALIC        (1 << 1)
+#define FP_FLAG_REVERSE       (1 << 2)
+#define FP_FLAG_UNDERCURL     (1 << 3)
+#define FP_FLAG_UNDERLINE     (1 << 4)
+#define FP_FLAG_STRIKETHROUGH (1 << 5)
+
+struct fpHlAttrs {
+    uint32_t foreground;
+    uint32_t background;
+    uint32_t special;
+    uint32_t flags;
+};
 struct fpUniforms {
     uint32_t width_px;
     uint32_t height_px;
     MEMBER_STRUCT fpAtlasUniforms font;
+    MEMBER_STRUCT fpHlAttrs attrs[256];
 };

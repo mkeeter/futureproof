cc2495d70b16de8b56a64838737933bd82af8096
Matt Keeter <matt.j.keeter@gmail.com>
2020-11-26 11:24:31 -0500

    Add infrastructure for Buffer API calls

--- src/buffer.zig
+++ src/buffer.zig
@@ -15,7 +15,34 @@ pub const Buffer = struct {
         // Nothing to do here yet
     }
 
+    pub fn api_changedtick_event(self: *Buffer, args: []const msgpack.Value) void {
+        std.debug.print("changedtick_event\n   ", .{});
+        for (args) |a| {
+            std.debug.print("{} ", .{a});
+        }
+        std.debug.print("\n", .{});
+    }
+
     pub fn rpc_method(self: *Buffer, name: []const u8, args: []const msgpack.Value) void {
-        std.debug.print("Buffer: unimplemented method {}\n", .{name});
+        var matched = false;
+
+        // Same trick as in tui.zig
+        comptime const opts = std.builtin.CallOptions{};
+        inline for (@typeInfo(Self).Struct.decls) |s| {
+            // This conditional should be optimized out, since
+            // it's known at comptime.
+            comptime const is_api = std.mem.startsWith(u8, s.name, "api_");
+            if (is_api) {
+                // Skip nvim_buf_ in the RPC name and api_ in the API name
+                if (std.mem.eql(u8, name[9..], s.name[4..])) {
+                    @call(opts, @field(Self, s.name), .{ self, args });
+                    matched = true;
+                    break;
+                }
+            }
+        }
+        if (!matched) {
+            std.debug.warn("[Buffer] Unimplemented API: {}\n", .{name});
+        }
     }
 };

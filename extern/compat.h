31cc048c5f07512d0313434a3a61d53851e053c0
Matt Keeter <matt.j.keeter@gmail.com>
2020-12-28 10:01:54 -0500

    Explain compat.h

--- extern/compat.h
+++ extern/compat.h
@@ -1,13 +1,17 @@
+// This is a shim which should be included in any header that's included
+// in both C / Zig and GLSL.  It allows headers to work around missing
+// sized integer types (in GLSL), vec types (in C), and slightly different
+// handling of struct naming (using the MEMBER_STRUCT macro).
+#pragma once
+
 #if GL_core_profile // Compiling as GLSL
 #define uint32_t uint
 #define int32_t int
 #define MEMBER_STRUCT
 
 #else // Compiling as a C header file
-#pragma once
 #include <stdint.h>
 #define MEMBER_STRUCT struct
-// Compiling as a C header file
 typedef struct {
     float x, y, z;
 } vec3;
